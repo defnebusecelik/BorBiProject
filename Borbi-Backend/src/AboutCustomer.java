03ecee5e0b45e3f74ef06788031c363db94a8469
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-12-13 01:28:21 +0300

    Add files via upload

---
+++ Borbi-Backend/src/AboutCustomer.java
@@ -0,0 +1,61 @@
+public class AboutCustomer {
+        private int customerId;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private String phoneNumber;
+
+
+        public int getCustomerId() {
+            return customerId;
+        }
+
+        public void setCustomerId(int customerId) {
+            this.customerId = customerId;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        @Override
+        public String toString() {
+            return "Customer:\n"+"Customer Id=\t" + customerId + "\nName=\t" + firstName + "\nLast Name=\t" + lastName + "\nE-mail=\t" + email + "\nPassword=\t" + password + "\nPhone Number=\t" + phoneNumber;
+        }
+    }
