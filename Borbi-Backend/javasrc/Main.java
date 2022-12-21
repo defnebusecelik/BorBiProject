7e01574f3de23970995a2c9a08796a21610a0a1e
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-12-21 16:33:48 +0300

    Create Main.java

---
+++ Borbi-Backend/javasrc/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        registerSwing i1=new Interface1();
+        flightSwing i2=new Interface2();
+        paymentSwing i3=new Interface3();
+        i1.setVisible(true);
+        i2.setVisible(true);
+        i3.setVisible(true);
+
+    }
+
+}
