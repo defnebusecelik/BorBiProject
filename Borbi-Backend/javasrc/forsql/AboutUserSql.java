0512cca5c034e9ad79e3df42f417f71cf509b1c9
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-12-21 15:13:11 +0300

    Update AboutUserSql.java

--- Borbi-Backend/javasrc/forsql/AboutUserSql.java
+++ Borbi-Backend/javasrc/forsql/AboutUserSql.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.*;
 import java.util.ArrayList;
 import java.util.List;
 import javasrc.model.AboutUser;
