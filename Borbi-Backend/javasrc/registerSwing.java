34af76365d57092c560b76800f8cb2a5f992670b
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-12-21 16:29:14 +0300

    Create registerSwing.java

---
+++ Borbi-Backend/javasrc/registerSwing.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Interface1 extends JFrame {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton loginButton;
+    private JButton registerButton;
+    private JPanel Panel1;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+
+    public Interface1(){
+        add(Panel1);
+        setSize(500,500);
+        setTitle("Flight");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        label2.setVisible(false);
+        textField2.setVisible(false);
+        Panel1.setVisible(true);
+
+        textField1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        textField2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        textField3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+}
