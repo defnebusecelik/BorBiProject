6b2b9b41771a61f9f09a5d4783d9d7cb0926562a
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-12-24 19:18:32 +0300

    Update Main.java

--- Borbi-Backend/javasrc/Main.java
+++ Borbi-Backend/javasrc/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        registerSwing i1=new Interface1();
-        flightSwing i2=new Interface2();
-        paymentSwing i3=new Interface3();
+        registerSwing i1=new registerSwing();
+        flightSwing i2=new flightSwing();
+        paymentSwing i3=new paymentSwing();
+        lastScene i4=new lastScene();
         i1.setVisible(true);
         i2.setVisible(true);
         i3.setVisible(true);
+        i4.setVisible(true);
 
     }
 
