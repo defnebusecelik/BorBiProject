4b51386a39bdfb1311b9335ac6fdca0d108f5dec
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-12-24 19:20:47 +0300

    Update registerSwing.java

--- Borbi-Backend/javasrc/registerSwing.java
+++ Borbi-Backend/javasrc/registerSwing.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class registerSwing extends JFrame {
     private JTextField textField1;
     private JTextField textField2;
@@ -13,36 +12,26 @@ public class registerSwing extends JFrame {
     private JLabel label1;
     private JLabel label2;
     private JLabel label3;
+    private JLabel icon1;
+    private JTextField textField4;
+    private JLabel label4;
+    private JButton joinUsButton;
 
     public registerSwing(){
+        ImageIcon icon=new ImageIcon("image/image.jpg");
+        setIconImage(icon.getImage());
+        icon1.setIcon(new ImageIcon("image/blue.jpg"));
         add(Panel1);
-        setSize(500,500);
+        setSize(600,600);
         setTitle("Flight");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        label2.setVisible(false);
-        textField2.setVisible(false);
+        label1.setVisible(false);
+        textField1.setVisible(false);
+        label4.setVisible(false);
+        textField4.setVisible(false);
+        registerButton.setVisible(false);
         Panel1.setVisible(true);
 
-        textField1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        textField2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        textField3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
 
         loginButton.addActionListener(new ActionListener() {
             @Override
@@ -55,6 +44,16 @@ public class registerSwing extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        joinUsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label1.setVisible(true);
+                textField1.setVisible(true);
+                label4.setVisible(true);
+                textField4.setVisible(true);
+                registerButton.setVisible(true);
             }
         });
     }
