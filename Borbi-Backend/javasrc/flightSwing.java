745800910a4eb380f15442f10db61cf3f7acdcc1
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-12-24 19:19:49 +0300

    Update flightSwing.java

--- Borbi-Backend/javasrc/flightSwing.java
+++ Borbi-Backend/javasrc/flightSwing.java
@@ -2,52 +2,58 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class flightSwing extends JFrame {
+public class registerSwing extends JFrame {
     private JTextField textField1;
-    private JComboBox comboBox1;
     private JTextField textField2;
     private JTextField textField3;
-    private JPanel Panel3;
-    private JButton buyButton;
+    private JButton loginButton;
+    private JButton registerButton;
+    private JPanel Panel1;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JLabel icon1;
+    private JTextField textField4;
+    private JLabel label4;
+    private JButton joinUsButton;
 
-    public flightSwing(){
-        add(Panel3);
-        setSize(500,500);
+    public registerSwing(){
+        ImageIcon icon=new ImageIcon("image/image.jpg");
+        setIconImage(icon.getImage());
+        icon1.setIcon(new ImageIcon("image/blue.jpg"));
+        add(Panel1);
+        setSize(600,600);
         setTitle("Flight");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        label1.setVisible(false);
+        textField1.setVisible(false);
+        label4.setVisible(false);
+        textField4.setVisible(false);
+        registerButton.setVisible(false);
+        Panel1.setVisible(true);
 
-        textField1.addActionListener(new ActionListener() {
+
+        loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
 
-        textField2.addActionListener(new ActionListener() {
+        registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
-
-        textField3.addActionListener(new ActionListener() {
+        joinUsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        buyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        comboBox1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+                label1.setVisible(true);
+                textField1.setVisible(true);
+                label4.setVisible(true);
+                textField4.setVisible(true);
+                registerButton.setVisible(true);
             }
         });
     }
